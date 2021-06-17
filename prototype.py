48eedd9701b9f37fe7875ca08b089d62850df3dc
Harith Onigemo <onigemotosin@gmail.com>
2021-06-18 01:51:14 +0400

    Added spawner

--- prototype.py
+++ prototype.py
@@ -1,14 +1,38 @@
-import turtle;
+import turtle
+import random
+import time
+
+
 screen = turtle.Screen()
 road_pen = turtle.Turtle()
-road_pen.penup()
-road_pen.goto(-100, -100)
-road_pen.pendown()
-road_pen.fd(100)
-road_pen.lt(90)
-road_pen.fd(200)
-road_pen.lt(90)
-road_pen.fd(100)
-road_pen.lt(90)
-road_pen.fd(200)
+
+oppCar1 = road_pen.clone()
+oppCar1.penup()
+oppCar1.shape("turtle")
+
+
+xCoor = [0, 58, 116]
+
+oppColours = ['red', 'green', 'yellow']
+
+def spawn(x):
+    start = time.time()
+    newColour = random.choice(oppColours)
+    oppCar1.setposition(x, 250)
+    oppCar1.setheading(-90)
+    oppCar1.color(newColour)
+    newX = random.choice(xCoor)
+    while True:
+        if time.time() - start > 2:
+            print(newX)
+            spawn(newX)
+            
+
+
+
+start = time.time()
+
+
+spawn(random.choice(xCoor))
+
 screen.exitonclick()
